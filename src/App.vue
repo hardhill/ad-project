c3de59b43a8e8b1d1b3c883c27b64f8dbca736e5
Waclaw Beletsky <hardhill@live.ru>
2019-08-26 20:14:56 +0800

    5 уроков

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer temporary v-model="drawer" absolute>
+      <v-list dense>
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url" link>
+          <v-list-item-icon>
+            <v-icon color="blue">{{link.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app dark class="primary">
+      <v-app-bar-nav-icon @click="drawer=!drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline">
+        <span>Vuetify</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text v-for="link of links" :key="link.title" :to="link.url">
+          <v-icon left>{{link.icon}}</v-icon>{{link.title}}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+      <v-footer app></v-footer>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      drawer: false,
+      links: [{ title: "Login", icon: "mdi-lock", link: "/login" },
+      { title: "Registration", icon: "mdi-face", link: "/registration" },
+      { title: "Orders", icon: "mdi-bookmark", link: "/orders" },
+      { title: "New ad", icon: "mdi-note", link: "/new" },
+      { title: "My ads", icon: "mdi-view-list", link: "/list" }]
+    };
+  }
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+      Home
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
