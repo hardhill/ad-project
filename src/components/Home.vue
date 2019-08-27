a0f0b60ce37d0ba9fe7a3dd3fe9d7c4ee66261de
Waclaw Beletsky <hardhill@live.ru>
2019-08-27 21:56:36 +0800

    commit 10 lesson

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,27 +5,26 @@
       <v-layout row>
         <v-flex xs12>
           <v-carousel>
-            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc"></v-carousel-item>
+            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc">
+              <div class="card-link">
+                <v-btn text :to="'/ad/'+ad.id">{{ad.title}}</v-btn>
+              </div>
+            </v-carousel-item>
           </v-carousel>
         </v-flex>
       </v-layout>
     </v-container>
     <v-container grid-list-lg>
       <v-layout row wrap="true">
-        <v-flex 
-        xs12
-        sm6
-        md4
-        lg3
-        v-for="ad in ads" :key="ad.id">
-          <v-card elevation="5" raised >
+        <v-flex xs12 sm6 md4 lg3 v-for="ad in ads" :key="ad.id">
+          <v-card elevation="5" raised>
             <v-img class="white--text" height="200px" :src="ad.imageSrc">
               <v-card-title class="align-end fill-height">{{ad.title}}</v-card-title>
             </v-img>
             <v-card-text>{{ad.description}}</v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn text>Open</v-btn>
+              <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
               <v-btn raised outlined="true">Buy</v-btn>
             </v-card-actions>
           </v-card>
@@ -75,5 +74,15 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style scoped>
+.card-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.33);
+}
+</style>>
--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,21 +1,52 @@
 <template>
   <v-container>
-      <v-layout row>
-        <v-flex>
-          <h1>Orders</h1>
-        </v-flex>
-      </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-list two-line flat>
+          <h1 class="text--secondary mb-3">Orders</h1>
+          <v-list-item-group multiple>
+            <v-list-item v-for="order in orders" :key="order.id">
+              <template>
+                <v-list-item-action>
+                  <v-checkbox :input-value="order.done" color="primary" @click="markDone(order)"></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>{{order.name}}</v-list-item-title>
+                  <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn class="primary" :to="'/ad/'+order.adId">Open</v-btn>
+                </v-list-item-action>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  data(){
-    return {}
+  data() {
+    return {
+      orders:[
+        { id:'dse2',
+          name:'Waclaw',
+          phone:'788-399-29-45',
+          adId:'1',
+          done:false
+          }
+      ]
+    };
+  },
+  methods:{
+      markDone(order){
+        order.done = true
+      }
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
